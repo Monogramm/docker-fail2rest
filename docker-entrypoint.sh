06d7ced93c4292ed6967a62f8645f77d766ed3b8
mathieu.brunot <mb.mathieu.brunot@gmail.com>
2019-03-13 00:33:17 +0100

    :pencil2: Fix typo in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,8 +6,8 @@ if [ ! -f /srv/fail2rest/config.json ]; then
 	echo "fail2rest configuration initialization..."
 
 	mkdir -p /srv/fail2rest/
-    echo \	
-		"{ \"Addr\": \"0.0.0.0:5000\", \"Fail2banSocket\": \"/var/run/fail2ban/fail2ban.sock\" }" \
+    echo \
+		'{ "Addr": "0.0.0.0:5000", "Fail2banSocket": "/var/run/fail2ban/fail2ban.sock" }' \
 		> /srv/fail2rest/config.json
 
 	echo "fail2rest configuration generated"
